0cb3adb4f1321cde3663ab3c2f02dba96ed678b5
Aryaman Pandey <aryuboxx@gmail.com>
2023-09-18 17:09:25 -0300

    Added comments to make the code more readable

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// Imports the important functions to use from firebase for the database to work properly
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+// Url of the actual database in firebase
 const appSettings = {
     databaseURL: "https://realtime-database-a820a-default-rtdb.firebaseio.com/"
 }
 
+// initialises everything so that the database can be used in the javascript file
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const shoppingListInDB = ref(database, "shoppingList");
@@ -15,6 +18,8 @@ const shoppingListEl = document.getElementById("shopping-list");
 
 const toggleThemeBtnEl = document.getElementById("toggle-theme-btn");
 
+// The function listens in on the addButton element and pushes the value inside the input using another function 
+// and clears the input button so that the user does not have to clear it themselves
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value;
     
@@ -23,11 +28,12 @@ addButtonEl.addEventListener("click", function() {
     clearInputFieldEl();
 })
 
+// The onValue function is responsible for checking the values in the database through the snapshot and then appending them to an array and 
 onValue(shoppingListInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val());
     
-        clearShoppingListEl();
+        clearShoppingListEl(); // We have to clear the shopping list element otherwise it adds the old elements again with the new elements.
         
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i];
@@ -49,7 +55,9 @@ function clearInputFieldEl() {
     inputFieldEl.value = "";
 }
 
+
 function appendItemToShoppingListEl(item) {
+    //adds the element into the list of items shown
     let itemID = item[0];
     let itemValue = item[1];
     
@@ -57,6 +65,7 @@ function appendItemToShoppingListEl(item) {
     
     newEl.textContent = itemValue;
     
+    //if the list element button is clicked then it asks if you wish to delete the item and then removes it if the user presses yes
     newEl.addEventListener("click", function() {
         
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
@@ -72,11 +81,13 @@ function appendItemToShoppingListEl(item) {
     shoppingListEl.append(newEl);
 }
 
+//this function sets the theme by using local storage so that it stores the theme and sets it using the function
 function setTheme(themeName) {
     localStorage.setItem("theme", themeName);
     document.documentElement.className = themeName;
 }
 
+//this function toggles the theme and sets it to the primaryTheme if it was on secondaryTheme
 function toggleTheme() {
     if (localStorage.getItem("theme") === "secondaryTheme") {
         setTheme("primaryTheme");
@@ -85,12 +96,13 @@ function toggleTheme() {
     }
 }
 
+//this function checks if the toggle button is clicked
 toggleThemeBtnEl.addEventListener("click", function() {
     toggleTheme();
 })
 
 
-
+//this checks what the value is stored in the Local Storage
 if (localStorage.getItem("theme") === "primaryTheme") {
     setTheme("primaryTheme");
 } else {
